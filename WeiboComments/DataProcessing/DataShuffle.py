306e65a1461b6e3e3943c952df88311c205d2a13
lx <lxsir2002@outlook.com>
2023-07-21 10:49:27 +0800

    重写微博语料的初始化

--- WeiboComments/DataProcessing/DataShuffle.py
+++ WeiboComments/DataProcessing/DataShuffle.py
@@ -1,33 +1,28 @@
-import csv
-import random
+import pandas as pd
+import re
 
-# 打开原始CSV文件并读取所有行
-with open('../data/simplifyweibo_4_moods.csv', 'r', encoding="utf-8") as infile:
-    reader = csv.reader(infile)
-    data = list(reader)
+# 打开CSV文件
+df = pd.read_csv('../data/SourceData/weibo_10.csv')
 
-row = data[0:]
-random.shuffle(row)
+# 提取第列名
+header = df.iloc[0]
+df = df[1:]
 
-# 计算出分成两份的行数，比例为7比3
-num_rows = len(row)
-num_rows_1 = int(num_rows * 0.7)
-num_rows_2 = num_rows - num_rows_1
+# 去除无效字符
+df['review'] = df['review'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
 
-# 将洗牌后的行分为两个列表
-train_data = row[:num_rows_1]
-test_data = row[num_rows_1:]
+# 打乱顺序
+df = df.sample(frac=1).reset_index(drop=True)
+
+# 计算切分的索引   7 比 3
+split_index = int(len(df) * 0.7)
+
+# 分割数据
+train_data = df[:split_index]
+test_data = df[split_index:]
 
 print(len(train_data))
 print(len(test_data))
 
-
-with open('../data/train_data.csv', 'w', newline='', encoding="utf-8") as file1:
-    writer = csv.writer(file1)
-    writer.writerow(['标签', '内容'])
-    writer.writerows(train_data)
-
-with open('../data/test_data.csv', 'w', newline='', encoding="utf-8") as file2:
-    writer = csv.writer(file2)
-    writer.writerow(['标签', '内容'])
-    writer.writerows(test_data)
+train_data.to_csv('../data/PreData/train_data.csv', index=False, columns=["review", "label"])
+test_data.to_csv('../data/PreData/test_data.csv', index=False, columns=["review", "label"])
