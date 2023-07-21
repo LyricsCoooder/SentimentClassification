f91857d1b8f94704ba6623eddf9c792a993f813a
Lyrics <lyrics528169@gmail.com>
2023-07-21 12:19:17 +0800

    更改三分类数据集为二分类，重写DataPreprocess.py，合并两数据集.

--- ProductReviews/DataProcessing/DataPreprocess.py
+++ ProductReviews/DataProcessing/DataPreprocess.py
@@ -1,51 +1,45 @@
 import csv
 
-def GetTextData(path):
-    with open(path, encoding="utf-8") as f:
-        dataLines = f.readlines()
+with open("../data/SourceData/data1.txt", encoding="utf-8") as f:
+    dataLines = f.readlines()
 
-    return dataLines
+contents = []
+labels = []
 
-# 将数据集根据 "point" 进行分割，point = 4 代表将数据集四等分，训练集占三等份，测试集占一等份
-def CutData(dataLines, point = 6):
-    breakPoint = int((point - 1) * len(dataLines) / point)
-    trainDataLines = dataLines[:breakPoint]
-    testDataLines = dataLines[breakPoint:]
-
-    return trainDataLines, testDataLines
-
-def GetContent_Label(dataLines):
-    contents = []
-    labels = []
-
-    for dataLine in dataLines:
-        dataLine = dataLine.strip()
-
-        label = int(dataLine[-1])
-        content = str(dataLine[:-1])
+for dataLine in dataLines:
+    dataLine = dataLine.strip()
+    label = int(dataLine[-1])
+    content = str(dataLine[:-1])
+    content = content.replace('\t','')
+    
+    if(label != 1):
+        if(label == 0):
+            label = 1
+        if(label == 2):
+            label = 0
         
-        content = content.replace('\t','')
-
         contents.append(content)
         labels.append(label)
 
-    return list(zip(contents, labels))
+csv_reader = csv.reader(open("../data/SourceData/data2.csv", 'r', encoding="utf-8"))
+for line in csv_reader:
+    line = str(line)[2:-2]
+    labels.append(1 - int(line[0]))
+    contents.append(line[3:])
 
-def TextToCsv(path, point = 6):
-    dataLines = GetTextData(path)
-    trainData, testData = CutData(dataLines, point)
+data = list(zip(labels, contents))
 
-    data = GetContent_Label(trainData)
-    with open('.\\data\\TrainData.csv', 'w', newline='', encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['内容', '标签'])
-        writer.writerows(data)
+testData = data[0:int(len(data)/10)]
+trainData = data[int(len(data)/10):]
 
-    data = GetContent_Label(testData)
-    with open('.\\data\\TestData.csv', 'w', newline='', encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['内容', '标签'])
-        writer.writerows(data)
-        
-TextToCsv(".\\data\\data.txt", point = 6)
+with open('../data/PreData/TrainData.csv', 'w', newline='', encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['标签','内容'])
+    writer.writerows(trainData)
+
+with open('../data/PreData/TestData.csv', 'w', newline='', encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['标签', '内容'])
+    writer.writerows(testData)
+
 print("DataPreprocess is ok!")
