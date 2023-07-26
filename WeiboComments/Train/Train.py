39f102db55fcc3aff9c7ae6e36bb320ecfa51569
Lyrics <lyrics528169@gmail.com>
2023-07-26 14:32:16 +0800

    断点训练与测试代码

--- WeiboComments/Train/Train.py
+++ WeiboComments/Train/Train.py
@@ -7,10 +7,11 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 input_size = 768
 hidden_size = 128
 num_layers = 2
-num_epochs = 5120
+num_epochs = 6656
 learning_rate = 0.01
 DropOut = 0.6
 num_classes = 2
+RESUME = True
 
 model = LSTM(input_size,
              hidden_size,
@@ -25,8 +26,18 @@ trainFiles = os.listdir("../data/PreData/TrainEmbeddings/")
 trainSum = int(len(trainFiles))
 trainNum = int(trainSum / 2)  # label embeddings 各占一半
 
+if RESUME:
+    path_checkpoint = "../models/checkpoint/ckpt.pth"  # 断点路径
+    checkpoint = torch.load(path_checkpoint)  # 加载断点
+    model.load_state_dict(checkpoint['net'])  # 加载模型可学习参数
+    optimizer.load_state_dict(checkpoint['optimizer'])  # 加载优化器参数
+    start_epoch = checkpoint['epoch']  # 设置开始的epoch
+    print(start_epoch)
+
 loss = 1
 for epoch in range(num_epochs):
+    if epoch <= start_epoch:
+        continue
     print("epoch" + str(epoch) + "is start!")
     for i in range(trainNum):
         data_train = np.load("../data/PreData/TrainEmbeddings/TrainDataEmbeddings" + str(i) + ".npy")
@@ -42,8 +53,16 @@ for epoch in range(num_epochs):
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-        print('%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+        print('%04d' % epoch, 'loss =', '{:.6f}'.format(loss))
 
-    print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+        checkpoint = {
+            "net": model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            "epoch": epoch
+        }
+        if not os.path.isdir("../models/checkpoint"):
+            os.mkdir("../models/checkpoint")
+        torch.save(checkpoint, '../models/checkpoint/ckpt.pth')
+
 
 torch.save(model, "../models/model.pth")
--- test/testModel.py
+++ test/testModel.py
@@ -1,20 +1,23 @@
 import torch
 import numpy as np
-import torch.nn as nn
+import os
+
+testFiles = os.listdir("../WeiboComments/data/PreData/TestEmbeddings/")
+testSum = int(len(testFiles))
+testNum = int(testSum / 2)  # label embeddings 各占一半
 
 model = torch.load("../WeiboComments/models/model.pth")
 model.eval()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# 测试集
-data_test = np.load("../WeiboComments/data/PreData/TestEmbeddings/TestDataEmbeddings0.npy")
-labels_test = np.load("../WeiboComments/data/PreData/TestEmbeddings/TestDataLabels0.npy")
+for i in range(testNum):
+    data_test = np.load("../WeiboComments/data/PreData/TestEmbeddings/TestDataEmbeddings" + str(i) + ".npy")
+    labels_test = np.load("../WeiboComments/data/PreData/TestEmbeddings/TestDataLabels" + str(i) + ".npy")
+    ans = []
+    for (features, labels) in zip(data_test, labels_test):
+        features_tensor = torch.tensor(features).float().to(device)
+        labels_tensor = torch.tensor(labels).long().to(device)
+        out = model(features_tensor)
+        ans.append(torch.sum(labels_tensor == torch.argmax(out, dim=1)) / 64)
 
-ans = []
-for i, (features, labels) in enumerate(zip(data_test, labels_test)):
-    features_tensor = torch.tensor(features).float().to(device)
-    labels_tensor = torch.tensor(labels).long().to(device)
-    out = model(features_tensor)
-    ans.append(torch.sum(labels_tensor == torch.argmax(out, dim=1)) / 64)
-
-print(ans)
+    print(sum(ans) / 128)
